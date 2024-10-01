087a2dd7324c4ceb1590ead93b63ef910b60fa1a
Ananya Mohapatra <ananyamohapatra27@gmail.com>
2024-10-01 18:44:59 +0530

    added files

---
+++ src/Components/CoffeeListComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CoffeeListComponent.css'
+const coffeeListComponent = () => {
+  return (
+    <div style={{zIndex:100}}>CoffeeListComponent</div>
+  )
+}
+
+export default coffeeListComponent
--- src/Components/MainWebComponent.js
+++ src/Components/MainWebComponent.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './MainWebComponent.css'
 import bgImg from '../assets/bg-cafe.jpg'
+import CoffeeListComponent from './CoffeeListComponent'
 const mainWebComponent = () => {
   return (
-    <div style={{backgroundColor:'black'}}>
+    <div style={{backgroundColor:'black',height:'100vh'}}>
         <img src={bgImg} className='bgImg' alt='background'/>
+        <CoffeeListComponent/>
     </div>
   )
 }
